9254912837257966a51daf39c2bb53be04cca546
vieiramanda11 <amanda_vieira@outlook.com>
2019-08-19 14:29:27 -0300

    adding player move method

--- lib/game.rb
+++ lib/game.rb
@@ -13,6 +13,7 @@ class Game
     @current_player = @player1
     @symbol1 = ''
     @symbol2 = ''
+    @end_round = false
   end
 
   def players(first, second)
@@ -22,5 +23,31 @@ class Game
 
   def change_player
     @current_player = @current_player == @player1 ? @player2 : @player1
+  end
+
+  def player_move
+    until @end_round
+      if @current_player == @player1
+        puts '#{@player1.name} Pick a place in the border choosing a number between 1 and 9 to put your symbol'
+        @player_input = gets.chomp.to_i
+        @player_input -= 1
+        if @board_game.board[@player_input].is_a? Integer
+          @board_game.board[@player_input] = @player1.symbol
+          @end_round = true
+        end
+      else
+        puts '#{@player2.name} Pick a place in the border choosing a number between 1 and 9 to put your symbol'
+        @player_input = gets.chomp.to_i
+        @player_input -= 1
+        if @board_game.board[@player_input].is_a? Integer
+          @board_game.board[player_input] = @player2.symbol
+          @end_round = true
+        end 
+      end
+    end
+    change_player
+    @board_game.draw_board
+    @board_game.legend
+    @end_round = false 
   end
 end
