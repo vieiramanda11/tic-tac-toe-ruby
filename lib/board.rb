b40982b74cda337a73b0778837510b592c749533
vieiramanda11 <amanda_vieira@outlook.com>
2019-08-22 19:26:42 -0300

    moving win? and draw? methods to board class

--- lib/board.rb
+++ lib/board.rb
@@ -16,4 +16,35 @@ class Board
     puts '-----------'
     puts " #{@board[6]} | #{@board[7]} | #{@board[8]}"
   end
+
+  def win_hor
+    if @board_game.board[0] == @board_game.board[1] && @board_game.board[0] == @board_game.board[2] ||
+       @board_game.board[3] == @board_game.board[4] && @board_game.board[3] == @board_game.board[5] ||
+       @board_game.board[6] == @board_game.board[7] && @board_game.board[6] == @board_game.board[8]
+      true
+    end
+  end
+
+  def win_dig
+    if @board_game.board[0] == @board_game.board[4] && @board_game.board[0] == @board_game.board[8] ||
+       @board_game.board[2] == @board_game.board[4] && @board_game.board[2] == @board_game.board[6]
+      true
+    end
+  end
+
+  def win_ver
+    if @board_game.board[0] == @board_game.board[3] && @board_game.board[0] == @board_game.board[6] ||
+       @board_game.board[1] == @board_game.board[4] && @board_game.board[1] == @board_game.board[7] ||
+       @board_game.board[2] == @board_game.board[5] && @board_game.board[2] == @board_game.board[9]
+      true
+    end
+  end
+
+  def draw?
+    false if @board_game.board.all? { |idx| idx.is_a?(Integer) }
+  end
+
+  def win?
+    true if win_dig || win_ver || win_hor
+  end
 end
