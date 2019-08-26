ec48fa7862f81d9d6289d39f55fbbc225c7272ae
vieiramanda11 <amanda_vieira@outlook.com>
2019-08-26 17:59:23 -0300

    adding tests for player class

---
+++ spec/player_spec.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+require_relative '../lib/player.rb'
+
+# Testing player class
+RSpec.describe Player do
+  let(:name) { 'Amanda' }
+  let(:symbol) { 'X' }
+  let(:player) { Player.new('Amanda', 'X') }
+
+  context "#initialize" do
+    it "when initialize the name should match" do
+      expect(player.player).to eql(name)
+    end
+
+    it "when initialize the symbol should match" do
+      expect(player.symbol).to eql(symbol)
+    end
+  end
+end
+
+# Testing input_player class
+RSpec.describe PlayerInput do
+  describe "#player_input" do
+    it "ha" do
+    end
+  end
+end
