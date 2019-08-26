0a48f30cb21edc29e02eefc68defb46136610abc
vieiramanda11 <amanda_vieira@outlook.com>
2019-08-26 17:47:06 -0300

    adding tests to Board class

---
+++ spec/board_spec.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+require_relative '../lib/board.rb'
+
+RSpec.describe Board do
+  let(:board) { Board.new }
+
+  let(:board_draw) {
+    puts "\n"
+    puts " 1 | 2 | 3"
+    puts '-----------'
+    puts " 4 | 5 | 6"
+    puts '-----------'
+    puts " 7 | 8 | 9"
+  }
+
+  describe "#initialize" do
+    it "should initialize as Board class" do
+      expect(board.class).to eql(Board)
+    end
+    
+    it "should make an array with length equals to 9" do
+      expect(board.board.length).to eql(9)
+    end
+  end
+
+  describe "#draw_board" do 
+    it "print the board in the command line" do
+      expect(board.draw_board).to eql(board_draw)
+    end
+  end 
+end
