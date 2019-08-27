65f5ec227473fed9a69e3dc2f7569e9cad480e87
vieiramanda11 <amanda_vieira@outlook.com>
2019-08-27 16:19:15 -0300

    adding empty lines for it blocks in tests files

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -17,30 +17,37 @@ RSpec.describe Game do
       game.board_game.board = %w[O O O 4 5 6 7 8 9]
       expect(game.win?).to eql(true)
     end
+
     it 'should return true when the second row of the board is filled with the same symbol' do
       game.board_game.board = %w[1 2 3 X X X 7 8 9]
       expect(game.win?).to eql(true)
     end
+
     it 'should return true when the third row of the board is filled with the same symbol' do
       game.board_game.board = %w[1 2 3 4 5 6 O O O]
       expect(game.win?).to eql(true)
     end
+
     it 'should return true when the first column of the board is filled with the same symbol' do
       game.board_game.board = %w[O 2 3 O 5 6 O 8 9]
       expect(game.win?).to eql(true)
     end
+
     it 'should return true when the second column of the board is filled with the same symbol' do
       game.board_game.board = %w[1 X 3 4 X 6 7 X 9]
       expect(game.win?).to eql(true)
     end
+
     it 'should return true when the third column of the board is filled with the same symbol' do
       game.board_game.board = %w[1 2 X 4 5 X 7 8 X]
       expect(game.win?).to eql(true)
     end
+
     it 'should return true when the first diagonal of the board is filled with the same symbol' do
       game.board_game.board = %w[O 2 3 4 O 6 7 8 O]
       expect(game.win?).to eql(true)
     end
+
     it 'should return true when the first diagonal of the board is filled with the same symbol' do
       game.board_game.board = %w[O 2 X 4 X 6 X 8 9]
       expect(game.win?).to eql(true)
